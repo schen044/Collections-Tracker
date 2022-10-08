24b6aa1cca22cc4099258794bc9bc524e054290c
schen044 <ninjarulers@yahoo.com>
2022-10-07 21:19:27 -0700

    Adds routing for OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// google oauth login route
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+  prompt: "select_account"
+}));
+
+// google oauth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies'
+  }
+));
+
+// oauth logout route
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/movies');
 });
 
 module.exports = router;
